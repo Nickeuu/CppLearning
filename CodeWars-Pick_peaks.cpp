5aa537a9d1216190df2e429b100262836a06d4c7
Nickeuu <nicolae.alexandru99@yahoo.com>
2023-04-02 10:38:47 +0000

    save

--- CodeWars-Pick_peaks.cpp
+++ CodeWars-Pick_peaks.cpp
@@ -18,37 +18,7 @@ PeakData pick_peaks(const std::vector<int> &v) {
     PeakData result;
     if (v.size() == 0) return result;
 
-    int val = 0, aux_val = 0;
-    bool isAscending = false, insertedVal = false;
-
-    for(unsigned long i = 0; i < v.size() - 1; i++){
-        val = v[i];
-        if(val < v[i+1]){
-            isAscending = true;
-        }else if(val > v[i+1]){
-            if(isAscending && val == v[i-1] && (i-1) > 0){
-                for (unsigned long j = i-1; j > 1; j--){
-                    if(val != v[j-1]){
-                        result.peaks.push_back(v[j]);
-                        result.pos.push_back(j);
-                        insertedVal = true;
-                        break;
-                    }
-                }
-            }else if(isAscending){
-                result.peaks.push_back(v[i]);
-                result.pos.push_back(i);
-                insertedVal = true;
-                isAscending = false;
-            }
-        }
-    }
-    if (!insertedVal){
-        if(v[1] > v[0] && v[v.size() - 1] < v[1]){
-            result.peaks.push_back(v[1]);
-            result.pos.push_back(1);
-        }
-    }
+    
 
     return result;
 }
