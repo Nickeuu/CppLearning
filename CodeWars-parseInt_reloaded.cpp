648cdfc0905bf46dd7cc9b6caf5a6b3809b1b55c
Nickeuu <nicolae.alexandru99@yahoo.com>
2023-04-03 17:08:02 +0000

    Added problem parseint reloaded

---
+++ CodeWars-parseInt_reloaded.cpp
@@ -0,0 +1,13 @@
+#include <string>
+#include <iostream>
+
+long parse_int(std::string number) {
+    for()
+    return 0;
+}
+
+
+int main(){
+    parse_int("two hundred and forty-six");
+    return 0;
+}
